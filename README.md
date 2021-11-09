d65fe97355aa4100762039e19ccdc80d4863b070
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2021-11-09 10:10:11 -0500

    0.0.1 Commit
    Heavily WIP.

--- README.md
+++ README.md
@@ -2,10 +2,87 @@
 Cool CLI tool to make shortcuts from Text.
 
 # Compatible with:
-Pretty much any device you can compile/execute C for (e.x. Windows, macOS, Linux, Android/iOS with a 3rd party app, etc.)
+Buttermilk is coded in 100% C, meaning you can run it on just about any device that can compile and execute C. Windows/macOS/Linux? Of course. iOS/Android? You'll need a third party app to compile it on-device, but yes. Your toaster? If it can compile/run C, likely yes.
 
 # Progress:
-HEAVILY WIP. Please don't use.
+Note: HEAVILY WIP.
+
+## Actions Implemented:
+OpenApp()
+
+GetURL()
+
+Alert()
+
+Ask()
+
+CreateFolder()
+
+FilterPhotos()
+
+Count()
+
+Print()
+
+GetItemFromList()
+
+Comment()
+
+Number()
+
+SaveFile()
+
+SetVol()
+
+Date()
+
+Flashlight()
+
+List()
+
+RunSSH()
+
+OpenURL()
+
+GetBattery()
+
+Nothing()
+
+Text()
+
+MakeGIF()
+
+GetVar()
+
+OpenIn()
+
+SetVar()
+
+SetAppearance()
+
+(You can also use the action ids from WFActions.plist if they aren't in actionnamesrev.json (although I highly don't recommend this), for example is.workflow.actions.downloadurl())
+
+
+## Not yet implemented features:
+-Smart Comments
+
+-Smart Multi-Line Comments
+
+-Smart Ifs
+
+-Smart Untils
+
+-Magic Variables
+
+-Action Parameters
+
+-Pretty much everything other than actions themselves
+
+# Convert output to Shortcut
+In Buttermilk's current state, you'll need to take the json it outputs and convert it to plist. Keep in mind this will change evetnually. I recommend this website https://wtools.io/convert-json-to-plist. Rename this plist to .shortcut, sign the file, import it and BOOM! You just made a shortcut with Buttermilk!
+
+# What about an app?
+Buttermilk is currently a CLI tool. While I was planning an iOS app as well, I don't know if I really have enough time anymore to make one. If I do eventually free up my schedule for an extended period of time, then you can expect a Buttermilk app. Don't count on it however.
 
 # Open Source:
-This project is open source under the GNUv3 license. It uses my SeaJson library, which is open source under the MIT license.
+This project is open source under the GPLv3 license. It also uses my SeaJson library, which is open source under the MIT license.
--- main.c
+++ main.c
@@ -4,62 +4,72 @@
 #include <string.h>
 #include "lib/seajson.h"
 
-int main() {
- int size = 0;
- int charindex = 0;
- int charindex2 = 0;
- int tempint = 0;
- FILE *fp;
+int main(void) {
+  char ch;
+  int size = 0;
+  int charindex = 0;
+  int charindex2 = 0;
+  int tempint = 0;
+  int instring = 0;
+  char tmpstring[100];
+  char wfactions[2000] = "{\"WFWorkflowClientVersion\":\"100\",\"WFWorkflowClientRelease\":\"3.0\",\"WFWorkflowMinimumClientVersion\":\"100\",\"WFWorkflowMinimumClientVersionString\":\"100\",\"WFWorkflowImportQuestions\":[],\"WFWorkflowTypes\":[\"NCWidget\",\"WatchKit\"],\"WFWorkflowInputContentItemClasses\":[\"WFAppStoreAppContentItem\",\"WFArticleContentItem\",\"WFContactContentItem\",\"WFDateContentItem\",\"WFEmailAddressContentItem\",\"WFGenericFileContentItem\",\"WFImageContentItem\",\"WFiTunesProductContentItem\",\"WFLocationContentItem\",\"WFDCMapsLinkContentItem\",\"WFAVAssetContentItem\",\"WFPDFContentItem\",\"WFPhoneNumberContentItem\",\"WFRichTextContentItem\",\"WFSafariWebPageContentItem\",\"WFStringContentItem\",\"WFURLContentItem\"],\"WFWorkflowIcon\":{\"WFWorkflowIconStartColor\":431817727,\"WFWorkflowIconGlyphNumber\":61440},\"WFWorkflowActions\":[";
+  char * actionid;
+  FILE *fp;
 
- //fp=fopen("testwrite.txt", "w");
- //fprintf(fp, "aba");
- //fclose(fp);
-
- fp = fopen("language/actionnamesrev.json", "r");
- fseek(fp, 0, SEEK_END); // seek to end of file
- size = ftell(fp); // get current file pointer
- fseek(fp, 0, SEEK_SET); // seek back to beginning of file
- char butternames[size];
- fscanf(fp, "%s", butternames);
- fclose(fp);
-
- fp = fopen("language/actioncontentrev.json", "r");
- fseek(fp, 0, SEEK_END); // seek to end of file
- size = ftell(fp); // get current file pointer
- fseek(fp, 0, SEEK_SET); // seek back to beginning of file
- char buttercontent[size];
- fscanf(fp, "%s", buttercontent);
- fclose(fp);
-
- fp = fopen("put/TestButter.butter", "r");
- fseek(fp, 0, SEEK_END); // seek to end of file
- size = ftell(fp); // get current file pointer
- fseek(fp, 0, SEEK_SET); // seek back to beginning of file
- char butter[size];
- fscanf(fp, "%s", butter);
- fclose(fp);
-
- while (tempint == 0){
-    if (butter[charindex] == '('){
-      tempint++;
-    } else {
+  fp = fopen("language/actionnamesrev.json", "r");
+  fseek(fp, 0, SEEK_END); // seek to end of file
+  size = ftell(fp); // get current file pointer
+  fseek(fp, 0, SEEK_SET); // seek back to beginning of file
+  char butternames[size];
+  fscanf(fp, "%s", butternames);
+  fclose(fp);
+ 
+  //fp=fopen("testwrite.txt", "w");
+  //fprintf(fp, "aba");
+  //fclose(fp);
+ 
+  fp = fopen("put/TestButter.butter", "r");
+  char line[100];
+  while (fgets(line, sizeof (line), fp)){
+    charindex2++;
+    charindex = 0;
+    tempint = 0;
+    while (tempint == 0){
+      if (line[charindex] == '('){
+        tempint++;
+      } else {
+      }
       charindex++;
     }
- }
- char actionname[charindex];
- tempint = 0;
- while (charindex2 < charindex) {
-   actionname[charindex2] = ("%c",butter[charindex2]);
-   charindex2++;
- }
- actionname[sizeof actionname] = '\0';
- charindex = 0;
- charindex2 = 0;
-
- // printf("Read Buffer: %s\n", buffer);
- char * actionid = getstring(actionname,butternames);
- char * actioncontent = getstring(actionname,buttercontent);
- printf("\n\n");
- printf("{\"WFWorkflowActions\":[{WFWorkflowActionIdentifier\":\"%s\",\"WFWorkflowActionParameters\":{},}]",actionid);
- return 0;
+    char params[charindex-1];
+    memcpy(params,&line[0],charindex-1);
+    params[charindex-1] = '\0';
+    charindex = 0;
+    tempint = 0;
+    if (charindex2 > 1){
+      strcat(wfactions,",");
+    }
+    strcat(wfactions,"{\"WFWorkflowActionIdentifier\":\"");
+    for(int i = 0; i < sizeof params; ++i) {
+      if (line[i] == '.'){
+        charindex++;
+      }
+    }
+    if (charindex == 0) {
+      actionid = getstring(params,butternames);
+    } else {
+      actionid = params;
+    }
+    printf("\n\n");
+    strcat(wfactions,actionid);
+    strcat(wfactions,"\",\"WFWorkflowActionParameters\":{\"UUID\":\"Buttermilk");
+    sprintf(tmpstring, "%d", charindex2);
+    strcat(wfactions,tmpstring);
+    strcat(wfactions,"\"}}");
+  }
+  strcat(wfactions,"]}");
+  system("clear");
+  printf("%s",wfactions);
+  fclose(fp);
+  return 0;
 }
